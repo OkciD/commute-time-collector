2acc694afd0ad7f4cd98a20f7481b886d7598b0e
Yury Baykov <y.baykov@corp.mail.ru>
2019-12-09 10:19:54 +0300

    copyright

--- install-chrome.sh
+++ install-chrome.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# Install Chrome.
+# Taken from https://gist.github.com/ziadoz/3e8ab7e944d02fe872c3454d17af31a5#file-install-sh-L25-L29
+
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google-chrome.list > /dev/null
 sudo apt-get -y update
