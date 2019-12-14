ddc401eb197751520885785d0d50355c6323f39a
Yury Baykov <y.baykov@corp.mail.ru>
2019-12-15 01:28:55 +0300

    configuring logs from command line

--- utils/args.ts
+++ utils/args.ts
@@ -1,9 +1,17 @@
 import minimist from 'minimist';
+import WebDriver from 'webdriver';
 
 interface Args extends minimist.ParsedArgs {
 	logsDir: string;
+	wdioLogLevel: WebDriver.WebDriverLogTypes,
+	chromedriverSilent: boolean,
 }
 
-const args: Args = minimist<Args>(process.argv.slice(2));
+const args: Args = minimist<Args>(process.argv.slice(2), {
+	default: {
+		wdioLogLevel: 'debug',
+		chromedriverSilent: false,
+	},
+});
 
 export default args;
--- utils/logger.ts
+++ utils/logger.ts
@@ -2,6 +2,7 @@ import winston from 'winston';
 import fecha from 'fecha';
 import args from './args';
 import packageJson from '../package.json';
+import WebDriver from 'webdriver';
 
 const LOGS_DIR: string = args.logsDir ?? packageJson.logsDir;
 
@@ -11,19 +12,18 @@ const id: string = Math.random().toString(36).substr(2, 7); // рандомны�
 export type Logger = winston.Logger;
 
 export function getChromedriverLogArg(): string | null {
-	if (process.env.NODE_ENV !== 'dev') {
+	if (process.env.NODE_ENV !== 'dev' || args.chromedriverSilent) {
 		return '--silent';
 	}
 
 	return null;
 }
 
-export function getWdioLogConfig(): { outputDir: string } | null {
-	if (process.env.NODE_ENV !== 'dev') {
-		return { outputDir: LOGS_DIR };
-	}
-
-	return null;
+export function getWdioLogConfig(): { logLevel: WebDriver.WebDriverLogTypes, outputDir?: string } {
+	return {
+		logLevel: args.wdioLogLevel,
+		...(process.env.NODE_ENV !== 'dev') && { outputDir: LOGS_DIR },
+	};
 }
 
 const prodLogger: Logger = winston.createLogger({
