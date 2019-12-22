fa8b284f4324b88e66107f7d176391ce6c894d8f
Yury Baykov <y.baykov@corp.mail.ru>
2019-12-22 23:45:49 +0300

    writing csv to file

--- lib/recordRoutesData.ts
+++ lib/recordRoutesData.ts
@@ -6,14 +6,15 @@ import fs from 'fs';
 import path from 'path';
 
 export default function recordRoutesData(outDir: string, routes: FilteredAutoRoute[], sessionData: SessionData) {
+	const outFilePath: string = path.join(outDir, `${sessionData.date}.csv`);
+	const resultsFileAlreadyExists: boolean = fs.existsSync(outFilePath);
+	const shouldGenerateHeader: boolean = !resultsFileAlreadyExists;
+
 	const csvRowsObjects: unknown[] = routes.map((route) => ({
 		// @ts-ignore
 		...flat.flatten(route),
 		sessionData,
 	}));
-
-	const resultsFileAlreadyExists: boolean = fs.existsSync(path.join(outDir, `${sessionData.time}.csv`));
-	const shouldGenerateHeader: boolean = !resultsFileAlreadyExists;
 
 	const csvString: string = csvStringify(csvRowsObjects, {
 		header: shouldGenerateHeader,
@@ -35,7 +36,5 @@ export default function recordRoutesData(outDir: string, routes: FilteredAutoRou
 		},
 	});
 
-	console.log('#'.repeat(10));
-	console.log(csvString);
-	console.log('#'.repeat(10));
+	fs.appendFileSync(outFilePath, csvString);
 }
