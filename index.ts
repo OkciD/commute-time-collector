aa24d690ab902e63a4e2b323436b01b0183f50c0
Yury Baykov <y.baykov@corp.mail.ru>
2020-01-26 02:04:20 +0300

    unified error handling

--- index.ts
+++ index.ts
@@ -8,10 +8,12 @@ import getRoutes from './lib/getRoutes';
 import recordRoutesData from './lib/recordRoutesData';
 
 process.addListener('unhandledRejection', (reason?: {} | null | Error) => {
-	logger.error('Unhandled rejection', { reason: (reason as Error)?.stack });
-	console.error(chalk.red('Unhandled rejection, reason: ', (reason as Error)?.stack));
-	logger.end();
+	const stack = (reason as Error)?.stack;
 
+	logger.error('Unhandled rejection', { reason: stack });
+	console.error(chalk.red('Unhandled rejection, reason: ', stack));
+
+	logger.end();
 	process.exit(1);
 });
 
@@ -24,12 +26,10 @@ async function main(): Promise<void> {
 
 	// todo: validate coords
 	if (!startCoords || !endCoords) {
-		console.error(chalk.red(
+		throw new Error(
 			'Either start or end coordinates are not found. ' +
 			'Please make sure you have provided both --startCoords and --endCoords params',
-		));
-
-		process.exit(1);
+		);
 	}
 
 	logger.info('Start');
