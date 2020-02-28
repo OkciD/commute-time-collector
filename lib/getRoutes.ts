5d52c77982c17c77aba9910d9396d562803a2810
Yury Baykov <y.baykov@corp.mail.ru>
2020-02-29 02:30:22 +0300

    getting routes via tor

--- lib/getRoutes.ts
+++ lib/getRoutes.ts
@@ -1,7 +1,8 @@
 import { Credentials } from './scrapeCredentials';
-import requestPromise from 'request-promise-native';
 import { createLocalLogger, CustomizedLogger } from '../utils/logger';
 import { AutoRoute, BuildRouteResponse, FilteredAutoRoute } from '../types';
+import callViaTor from '../utils/callViaTor';
+import request from 'request';
 
 const localLogger: CustomizedLogger = createLocalLogger(module);
 
@@ -12,8 +13,8 @@ export default async function getRoutes(
 ): Promise<FilteredAutoRoute[]> {
 	const { csrfToken, sessionId, cookieJar } = credentials;
 
-	const options: requestPromise.OptionsWithUri = {
-		uri: 'https://yandex.ru/maps/api/router/buildRoute',
+	const options: request.OptionsWithUrl = {
+		url: 'https://yandex.ru/maps/api/router/buildRoute',
 		method: 'GET',
 		qs: {
 			ajax: 1,
@@ -31,30 +32,9 @@ export default async function getRoutes(
 
 		jar: cookieJar,
 		json: true,
-
-		resolveWithFullResponse: true,
-		time: true,
-		simple: false,
 	};
 
-	localLogger.debug('Request options', { options });
-
-	const response: requestPromise.FullResponse = await requestPromise(options);
-	const { statusCode, timingPhases, body } = response;
-
-	localLogger.performance('API', timingPhases);
-
-	if (statusCode !== 200 || body.error) {
-		localLogger.error('Request failed', { response: response.toJSON() });
-		throw new Error('Failed to get routes');
-	}
-
-	localLogger.debug('Request succeeded', {
-		response: {
-			...response,
-			body: '<omitted>',
-		}.toJSON(),
-	});
+	const { body }: request.Response = await callViaTor(options);
 
 	const { data } = body as BuildRouteResponse;
 	const filteredRoutes: FilteredAutoRoute[] = data.routes.map((route: AutoRoute) => ({
