0eaa4ffb29df4792ed79be7fc609fd5f3a54eccb
Yury Baykov <y.baykov@corp.mail.ru>
2020-02-28 20:37:40 +0300

    parsing html to get config json

--- lib/scrapeCredentials.ts
+++ lib/scrapeCredentials.ts
@@ -2,6 +2,7 @@ import * as Webdriver from 'webdriver';
 import request from 'request';
 // @ts-ignore
 import torRequest from 'tor-request';
+import cheerio from 'cheerio';
 import { createLocalLogger, CustomizedLogger } from '../utils/logger';
 
 export interface Credentials {
@@ -31,11 +32,20 @@ const localLogger: CustomizedLogger = createLocalLogger(module);
 export default async function scrapeCredentials(): Credentials {
 	const cokieJar: request.CookieJar = request.jar();
 
+	torRequest.setTorAddress('127.0.0.1', 9050);
 	torRequest.request('https://yandex.ru/maps/', {
 		jar: cokieJar,
-	}, (err: unknown, res: request.Response /* , body: unknown */) => {
-		if (!err && res.statusCode === 200) {
-			console.log(cokieJar.getCookieString('https://yandex.ru/maps'));
+	}, (err: unknown, res: request.Response, body: string) => {
+		if (err || res.statusCode !== 200) {
+			console.error(err);
+			return;
 		}
+
+		console.log(body);
+		const $ = cheerio.load(body);
+		const configView = $('script.config-view').get()[0];
+		const json = configView.children[0].data;
+
+		console.log(JSON.stringify(JSON.parse(json), null, 2));
 	});
 }
