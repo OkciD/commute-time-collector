b1ebdd5e91ce19414c596d6aa38b5247c38121ce
Yury Baykov <y.baykov@corp.mail.ru>
2020-02-28 21:28:30 +0300

    removed unnecessary script

--- install-chrome.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Taken from https://gist.github.com/ziadoz/3e8ab7e944d02fe872c3454d17af31a5#file-install-sh-L25-L29
-
-sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google-chrome.list > /dev/null
-sudo apt-get -y update
-sudo apt-get -y install google-chrome-stable
