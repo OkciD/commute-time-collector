76e07d999b1da7b200cfa08d6d84c656968fdbab
Yury Baykov <y.baykov@corp.mail.ru>
2020-02-28 14:38:30 +0300

    upd deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports =  {
 	parser:  '@typescript-eslint/parser',
 	extends: ['airbnb-typescript/base'],
 	parserOptions:  {
+		project: './tsconfig.json',
 		ecmaVersion:  2019,
 		sourceType: 'module',
 	},
