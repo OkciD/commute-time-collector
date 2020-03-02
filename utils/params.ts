2f1ed56ef0cf206961b873aa43dff4ef7519014a
Yury Baykov <y.baykov@corp.mail.ru>
2020-03-03 00:03:06 +0300

    added cronExpression param

--- utils/params.ts
+++ utils/params.ts
@@ -2,17 +2,22 @@ import minimist from 'minimist';
 import path from 'path';
 
 interface Params extends minimist.ParsedArgs {
-	logsDir: string;
 	startCoords: string;
 	endCoords: string;
+
+	logsDir: string;
 	outDir: string;
+
 	torHost: string;
 	torPorts: string;
+
+	cronExpression: string;
 }
 
 const parsedParams: Params = minimist<Params>(process.argv.slice(2), {
 	default: {
 		logsDir: path.resolve('logs'),
+
 		torHost: '127.0.0.1',
 		torPorts: '9050',
 	},
