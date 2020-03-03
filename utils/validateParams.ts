6c854b08ae0f52e706ce50d00342c54652f92524
Yury Baykov <y.baykov@corp.mail.ru>
2020-03-03 11:54:39 +0300

    added logging to params validation & enhanced tor params checking

--- utils/validateParams.ts
+++ utils/validateParams.ts
@@ -2,6 +2,12 @@ import params from './params';
 import assert from 'assert';
 import cron from 'node-cron';
 import tcpPortUsed from 'tcp-port-used';
+import { createLocalLogger, CustomizedLogger } from './logger';
+
+const CHECK_PORT_RETRY_INTERVAL = 500;
+const CHECK_PORT_TIMEOUT = 3000;
+
+const localLogger: CustomizedLogger = createLocalLogger(module);
 
 export default async function validateParams(kek: typeof params) {
 	const { startCoords, endCoords, cronExpression, torPorts, torHost } = kek;
@@ -10,16 +16,29 @@ export default async function validateParams(kek: typeof params) {
 	[startCoords, endCoords].forEach((coords) => {
 		assert.notEqual(typeof coords, 'undefined', 'Params startCoords & endCoords are required');
 	});
+	localLogger.debug('startCoords & endCoords ok');
 
 	// проверяем валидность cron-выражения встроенным в пакет node-cron валидатором
 	assert.ok(
 		cron.validate(cronExpression),
 		'Invalid cronExpression param. See: https://github.com/node-cron/node-cron',
 	);
+	localLogger.debug('cronExpression ok');
 
-	await Promise.all(torPorts.map((torPort: string) =>
-		tcpPortUsed
-			.check(+torPort, torHost)
-			.then((isUsed: boolean) => assert.ok(isUsed, `${torHost}:${torPort} is not occupied by any process`)),
-	));
+	await Promise.all(torPorts.map((torPort: string) => {
+		localLogger.debug(`Checking access to ${torHost}:${torPort}`);
+
+		return tcpPortUsed
+			.check({
+				port: +torPort,
+				host: torHost,
+				status: true,
+				retryTimeMs: CHECK_PORT_RETRY_INTERVAL,
+				timeOutMs: CHECK_PORT_TIMEOUT,
+			})
+			.then((isUsed: boolean) => {
+				assert.ok(isUsed, `${torHost}:${torPort} is not occupied by any process`);
+				localLogger.debug(`${torHost}:${torPort} is accessible`);
+			});
+	}));
 }
