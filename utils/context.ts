750dd9189036b0305038d6543b83930c92277283
Yury Baykov <y.baykov@corp.mail.ru>
2020-03-06 21:00:13 +0300

    removed tor ports validation :(

--- utils/context.ts
+++ utils/context.ts
@@ -4,8 +4,6 @@ import { Merge } from 'type-fest';
 import path from 'path';
 import assert from 'assert';
 import cron from 'node-cron';
-// import tcpPortUsed from 'tcp-port-used';
-import fs from 'fs';
 
 interface RawParams extends minimist.ParsedArgs {
 	startCoords: string;
@@ -64,43 +62,18 @@ class Context {
 	}
 
 	private static validateParams(params: Params): void {
-		const { startCoords, endCoords, cronExpression, /* torPorts, torHost, */ logsDir, outDir } = params;
+		const { startCoords, endCoords, cronExpression } = params;
 
 		// todo: проверять формат startCoords и endCoords
 		[startCoords, endCoords].forEach((coords) => {
 			assert.notEqual(typeof coords, 'undefined', 'Params startCoords & endCoords are required');
 		});
-		// console.log('startCoords & endCoords ok');
 
 		// проверяем валидность cron-выражения встроенным в пакет node-cron валидатором
 		assert.ok(
 			cron.validate(cronExpression),
 			'Invalid cronExpression param. See: https://github.com/node-cron/node-cron',
 		);
-		// console.log('cronExpression ok');
-
-		// проверяем доступность хоста и портов tor - все порты должны быть заняты
-		// await Promise.all(torPorts.map((torPort: string) => tcpPortUsed
-		// 	.check({
-		// 		port: +torPort,
-		// 		host: torHost,
-		// 		status: true,
-		// 		retryTimeMs: Context.CheckPortRetryInterval,
-		// 		timeOutMs: Context.CheckPortTimeout,
-		// 	})
-		// 	.then((isUsed: boolean) => {
-		// 		assert.ok(isUsed, `${torHost}:${torPort} is not occupied by any process`);
-		// 		// console.log(`${torHost}:${torPort} is accessible`);
-		// 	}),
-		// ));
-
-		// проверяем доступ на запись в outDir
-		fs.accessSync(outDir, fs.constants.W_OK);
-		// console.log('outDir is permitted to write');
-
-		// проверяем доступ на запись в logsDir
-		fs.accessSync(logsDir, fs.constants.W_OK);
-		// console.log('logsDir is permitted to write');
 	}
 }
 
