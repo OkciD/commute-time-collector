6c5b1dd19f0a2acba7a8310d914b568319dd676b
Yury Baykov <y.baykov@corp.mail.ru>
2020-03-06 11:59:59 +0300

    cron for prod only

--- index.ts
+++ index.ts
@@ -4,33 +4,38 @@ import { measuredAsyncFn, measuredSyncFn } from './utils/performance';
 import { FilteredAutoRoute } from './types';
 import getRoutes from './lib/getRoutes';
 import recordRoutesData from './lib/recordRoutesData';
-// import cron from 'node-cron';
+import cron from 'node-cron';
 import context from './utils/context';
 
 async function main(): Promise<void> {
-	context.reload();
+	try {
+		context.reload();
 
-	logger.info('Start', { params: context.params });
-	const { startCoords, endCoords, outDir } = context.params;
+		logger.info('Start', { params: context.params });
+		const { startCoords, endCoords, outDir } = context.params;
 
-	const credentials: Credentials = await measuredAsyncFn(scrapeCredentials)();
-	logger.info('Successfully scraped credentials from the page');
+		const credentials: Credentials = await measuredAsyncFn(scrapeCredentials)();
+		logger.info('Successfully scraped credentials from the page');
 
-	const routes: FilteredAutoRoute[] = await getRoutes(startCoords, endCoords, credentials);
-	logger.info('Successfully fetched routes data');
+		const routes: FilteredAutoRoute[] = await getRoutes(startCoords, endCoords, credentials);
+		logger.info('Successfully fetched routes data');
 
-	measuredSyncFn(recordRoutesData)(outDir, routes);
-	logger.info('Routes data has been written to a file');
+		measuredSyncFn(recordRoutesData)(outDir, routes);
+		logger.info('Routes data has been written to a file');
 
-	logger.info('End');
-}
-
-// cron.schedule('* * * * *', () => {
-measuredAsyncFn(main)()
-	.catch((error?: {} | null | Error) => {
+		logger.info('End');
+	} catch (error) {
 		logger.error(error ?? 'Unknown error');
 
 		logger.end();
 		process.exit(1);
+	}
+}
+
+if (process.env.NODE_ENV === 'dev') {
+	measuredAsyncFn(main)();
+} else {
+	cron.schedule(context.params.cronExpression, () => {
+		measuredAsyncFn(main)();
 	});
-// });
+}
